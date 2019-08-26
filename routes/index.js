dccc6456cdc8d2056386ccd50c6d28e0af8d7dbe
Roicel Laya <rlaya@autentia.cl>
2019-08-25 23:51:48 -0400

    Added post route with dummy response.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({ title: 'Express' });
+});
+
+// Allow CORS to post requests
+router.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type, x-user-browser, x-user-os');
+  next();
+});
+
+router.post('/', function(req, res, next) {
+  res.status(200).json({ success: true });
 });
 
 module.exports = router;
