3116be651dde784ccd4be62d70a4cc14c0ea64c4
Roicel Laya <rlaya@autentia.cl>
2019-08-26 21:12:41 -0400

    User data saved to data.txt file.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var fs = require('fs');
+var util = require('util');
 var router = express.Router();
 var middlewares = require('../middlewares/validate-fields');
+var writeFile = util.promisify(fs.writeFile);
 
 router.get('/', function(req, res, next) {
   res.status(200).json({ title: 'Express' });
@@ -19,8 +22,18 @@ router.post('/',
   middlewares.validateEmail,
   middlewares.validateIncome,
   function(req, res, next) {
+    let data = 'Rut\t\t' + req.body.rut + '\n';
+    data = data + 'Cellphone\t' + req.body.cellphone + '\n';
+    data = data + 'Email\t\t' + req.body.email + '\n';
+    data = data + 'Income\t\t' + req.body.income + '\n';
     // Save data on text file
-    res.status(200).json({ success: true });
+    writeFile('./data.txt', data)
+      .then(function() {
+        res.status(200).json({ message: 'Data saved successfully.' })
+      })
+      .catch(function(error) {
+        res.status(400).json({ message: 'Error saving data, please try again.' });
+      })
   }
 );
 
